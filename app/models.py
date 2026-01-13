0dc25dddf19935d8fa1eea04247486ef646c2b93
DayForSunshine <hhshanniwu@gmail.com>
2026-01-13 21:56:00 +0800

    Update models.py

--- app/models.py
+++ app/models.py
@@ -15,14 +15,14 @@ class JobRequest(BaseModel):
     #options: JobInputOptions
 
     @field_validator('input') #校验input字段
-    def validate_input(cls, v): #cls表示当前类本身（JobRequest）， v待校验的值input
-        if 'numbers' not in v:
-            raise ValueError("必须包含 'numbers' 字段") #Pydantic捕获ValueError包装成RequestValidationError，FastAPI捕获RequestValidationError然后将状态码设为422和错误信息一起塞进响应体发回msg字段显示
-        if not isinstance(v['numbers'], list):
+    def validate_input(cls, v): #cls表示当前类本身（JobRequest）， v待校验的值input。数据已经由JSON被转换成JobRequest对象所以不能字典方式读取numbers。
+       if not v.numbers:        
+            raise ValueError("必须包含 'numbers' 字段") #Pydantic捕获ValueError包装成RequestValidationError，FastAPI捕获RequestValidationError然后将状态码设为422和错误信息一起塞进响应体发回msg字段显示。
+        if not isinstance(v.numbers, list):
             raise ValueError("'numbers' 必须是一个列表")
-        if len(v['numbers']) == 0:
+        if not v.numbers:
             raise ValueError("'numbers' 列表不能为空")
-        return v #如果不返回 v，Pydantic 会认为这个字段的值是 None，导致数据丢失。
+        return v #如果不返回v，Pydantic会认为这个字段的值是None，导致数据丢失。
 
 # Job Status + Progress Response
 class JobProgress(BaseModel):
